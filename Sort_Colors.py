3ecb12f7e19560b67ae10068c1a96b22462fb721
deepti-talesra <66984808+deepti-talesra@users.noreply.github.com>
2020-07-28 01:06:12 -0400

    Create Sort_Colors.py

---
+++ Sort_Colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def sortColors(self, nums: List[int]) -> None:
+        red = 0
+        white = 0
+        blue = len(nums) - 1
+        
+        while white <= blue:
+            curr = nums[white]
+            if curr == 0:
+                nums[white] = nums[red]
+                nums[red] = 0
+                red += 1
+                white += 1
+            elif curr == 1:
+                white += 1
+            else:
+                nums[white] = nums[blue]
+                nums[blue] = 2
+                blue -= 1
