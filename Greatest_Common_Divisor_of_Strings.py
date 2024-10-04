10c8996821abf11a430dd836895d2259de8e986f
Deepti Talesra <66984808+deepti-talesra@users.noreply.github.com>
2024-10-04 17:40:54 -0400

    Create Greatest_Common_Divisor_of_Strings.py

---
+++ Greatest_Common_Divisor_of_Strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        length = math.gcd(len(str1), len(str2))
+        return str1[:length]
